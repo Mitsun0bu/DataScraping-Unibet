baea7d75a9dfb0d24528ea7b2c8160688e3fc6fb
Lucas Lethuillier <lucas.lethuillier@gmail.com>
2022-03-14 01:13:05 +0100

    chromdriver updated | selection of games of interest implemented

--- unibet_scraping.py
+++ unibet_scraping.py
@@ -41,48 +41,54 @@
 #																			   #
 # **************************************************************************** #
 
-# ~								IMPORT LIBRAIRIES							 ~ #
+# ============ IMPORT LIBRAIRIES ================== #
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
-#from prettytable import PrettyTable
 
-# ~								REACH WEBSITE								 ~ #
+# ================================= REACH WEBSITE ================================= #
 url = 'https://unibet.fr/sport/football/ligue-1-ubereats'
-s = Service('/Users/lucas/Documents/MY_PYTHON_PROJECTS/chromedriver')
+s = Service('/Users/lucas/Documents/MY_PYTHON_PROJECTS/unibet_scraping/chromedriver')
 driver = webdriver.Chrome(service=s)
 driver.get(url)
 
-# ~								CLICK COOKIE BUTTON					 		 ~ #
+# ==================================== CLICK COOKIE BUTTON ===================================== #
 time.sleep(5)
 accept = driver.find_element(By.XPATH, '//a[@class="ui-button ui-large ui-important link-track"]')
 accept.click()
 
-# ~								INITIALIZATION								 ~ #
-games = []
+# === INITIALIZATION ===#
 n_games = 0
-odds = []
+all_games = []
+good_games = []
+all_odds = []
 draw_odds = []
+good_odds  = []
 
-# ~								GETTING 'GAMES' 							 ~ #
+# ===================== GETTING 'ALL_GAMES' LIST ====================== #
 for game in driver.find_elements(By.XPATH, '//div[@class="cell-event"]'):
-	games.append(game.text)
-n_games = len(games)
+	all_games.append(game.text)
+n_games = len(all_games)
 
-# ~								GETTING 'DRAW_ODDS'								 ~ #
+# ==================================== GETTING 'DRAW_ODDS' LIST ==================================== #
 for odd in driver.find_elements(By.XPATH, '//span[@class="ui-touchlink-needsclick price odd-price"]'):
-	odds.append(odd.text)
+	all_odds.append(odd.text)
 for i in range(1, (n_games * 3 - 1), 3):
-	draw_odds.append(odds[i])
+	draw_odds.append(all_odds[i])
 
-# ~								QUIT WEBSITE								 ~ #
+# === QUIT WEBSITE=== #
 driver.quit()
 
-# ~								STORING LISTS IN DICT_BET					 ~ #
-dict_bet = {'Games': games, 'Draw Odds': draw_odds}
+# ========================== KEEPING GAMES WITH 2.80 < DRAW ODDS 3.50 ========================== #
+for i in range(0, n_games, 1):
+	if float(draw_odds[i]) > 2.80 and float(draw_odds[i]) < 3.50 : good_odds.append(draw_odds[i])
+	if float(draw_odds[i]) > 2.80 and float(draw_odds[i]) < 3.50 : good_games.append(all_games[i])
 
-# ~								SHOW DATA IN DATAFRAME						 ~ #
-df_bet = pd.DataFrame.from_dict(dict_bet)
-print(df_bet)
+# ==== CREATING A DICTIONNARY W/ DATA OF INTEREST ==== #
+bet_dico = {'Games': good_games, 'Draw Odds': good_odds}
+
+# ========== SHOW DATA IN DATAFRAME ========== #
+bet_dataframe = pd.DataFrame.from_dict(bet_dico)
+print(bet_dataframe)
